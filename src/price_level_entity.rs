b40ada29acf12bb276482748f9dc83d43a015105
amigin <amigin@gmail.com>
2025-03-25 11:14:27 +0200

    Naming Fix

--- src/price_level_entity.rs
+++ src/price_level_entity.rs
@@ -6,6 +6,6 @@ service_sdk::macros::use_my_no_sql_entity!();
 //RowKey - g: global, 1m: minute, 1h: hour etc. Consts are in ws_contracts library
 #[my_no_sql_entity("price-levels")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct PriceLevelEntity {
+pub struct PriceLevelMyNoSqlEntity {
     pub price: f64,
 }
