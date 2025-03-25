0a7feaffbd5064f6f20ce718b45e20526f2fd56b
amigin <amigin@gmail.com>
2025-03-25 16:45:36 +0200

    Minor Fixes with Price Levels

--- src/price_level_entity.rs
+++ src/price_level_entity.rs
@@ -6,6 +6,24 @@ service_sdk::macros::use_my_no_sql_entity!();
 //RowKey - g: global, 1m: minute, 1h: hour etc. Consts are in ws_contracts library
 #[my_no_sql_entity("price-levels")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct PriceLevelMyNoSqlEntity {
-    pub price: f64,
+pub struct PriceLevelMyNoSqlEntity {}
+
+impl PriceLevelMyNoSqlEntity {
+    pub fn new(instrument_id: String, tp: &str, price: f64) -> Self {
+        Self {
+            partition_key: instrument_id,
+            row_key: format!("{}:{}", tp, price),
+            time_stamp: Default::default(),
+        }
+    }
+    pub fn get_type(&self) -> &str {
+        let index = self.row_key.find(":").unwrap();
+        &self.row_key[..index]
+    }
+
+    pub fn get_price(&self) -> f64 {
+        let index = self.row_key.find(":").unwrap();
+        let as_str = &self.row_key[index + 1..];
+        as_str.parse().unwrap()
+    }
 }
