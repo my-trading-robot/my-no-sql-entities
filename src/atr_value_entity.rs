f7f49206ec93afc51b3f9353e0857e49d4900b72
amigin <amigin@gmail.com>
2025-03-29 00:42:00 +0200

    Fixes

--- src/atr_value_entity.rs
+++ src/atr_value_entity.rs
@@ -11,12 +11,6 @@ pub struct AtrValueMyNoSqlEntity {
 }
 
 impl AtrValueMyNoSqlEntity {
-    pub const MIN_CANDLE_TYPE: &'static str = "1m";
-    pub const MIN_5_CANDLE_TYPE: &'static str = "5m";
-    pub const HOUR_CANDLE_TYPE: &'static str = "1h";
-    pub const DAY_CANDLE_TYPE: &'static str = "1d";
-    pub const MONTH_CANDLE_TYPE: &'static str = "1M";
-
     pub fn get_instrument(&self) -> &str {
         &self.partition_key
     }
---
+++ src/consts.rs
@@ -0,0 +1,5 @@
+pub const MIN_CANDLE_TYPE: &'static str = "1m";
+pub const MIN_5_CANDLE_TYPE: &'static str = "5m";
+pub const HOUR_CANDLE_TYPE: &'static str = "1h";
+pub const DAY_CANDLE_TYPE: &'static str = "1d";
+pub const MONTH_CANDLE_TYPE: &'static str = "1M";
