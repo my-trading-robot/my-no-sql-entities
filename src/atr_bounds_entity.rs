e90d40925c7d677746bfad13fb6d8a4b5ace2bcb
Maxim Gorin <onlinexmax@gmail.com>
2025-03-29 10:12:02 +0300

    Add more fields to atr-bounds

--- src/atr_bounds_entity.rs
+++ src/atr_bounds_entity.rs
@@ -9,9 +9,10 @@ service_sdk::macros::use_my_no_sql_entity!();
 pub struct AtrBoundsEntity {
     pub ignore_height_above: f64,
     pub ignore_height_below: f64,
+    pub candle_date_from: String,
+    pub candle_date_to: String,
     pub candles_count: usize,
-    pub upper_index: usize,
-    pub lower_index: usize,
-    pub date_from: String,
-    pub date_to: String,
+    pub heights_count: usize,
+    pub upper_height_index: usize,
+    pub lower_height_index: usize,
 }
