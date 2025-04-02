51e2110fc390e0a0429fe7cff1c6fe6aef1ff446
Maxim Gorin <onlinexmax@gmail.com>
2025-04-02 21:13:31 +0300

    Change row key

--- src/near_level_instrument_entity.rs
+++ src/near_level_instrument_entity.rs
@@ -5,23 +5,23 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[my_no_sql_entity("near-level-instruments")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct NearLevelInstrumentMyNoSqlEntity {
-    pub current_price: f64,
-    pub result_price: f64,
+    pub atr_value: f64,
+    pub bid_ask_price: f64,
 }
 
 impl NearLevelInstrumentMyNoSqlEntity {
-    pub fn generate_rk(atr: f64, level_price: f64, level_type: &str) -> String {
-        format!("{atr}:{level_type}:{level_price}")
+    pub fn generate_rk(candle_type: &str, level_type: &str, level_price: f64) -> String {
+        format!("{candle_type}:{level_type}:{level_price}")
     }
 
     pub fn get_instrument_id(&self) -> &str {
         &self.partition_key
     }
 
-    pub fn get_atr(&self) -> f64 {
+    pub fn get_candle_type(&self) -> &str {
         let splits = self.row_key.split(":").collect::<Vec<&str>>();
 
-        splits[0].parse().unwrap()
+        splits[0]
     }
 
     pub fn get_level_type(&self) -> &str {
