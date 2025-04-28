829bfc94bf08bee45d74e1fe4d04e1983c326ab5
yuriy-smplt <pluriw@gmail.com>
2025-04-28 20:24:59 +0300

    renaming LimitTraderNoSqlEntity

--- src/limit_trader.rs
+++ src/limit_trader.rs
@@ -4,14 +4,14 @@ service_sdk::macros::use_my_no_sql_entity!();
 
 //Partition - Instrument_id
 //RowKey - Interval ("1h", "1d")
-#[my_no_sql_entity("limit-player")]
+#[my_no_sql_entity("limit-trader")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct LimitPlayerNoSqlEntity {
+pub struct LimitTraderNoSqlEntity {
     pub level: f64,               
-    pub side: LimitPlayerSideMyNoSql, 
+    pub side: LimitTraderSideMyNoSql, 
 }
 
-impl LimitPlayerNoSqlEntity {
+impl LimitTraderNoSqlEntity {
     pub fn get_instrument(&self) -> &str {
         &self.partition_key
     }
@@ -23,12 +23,12 @@ impl LimitPlayerNoSqlEntity {
 
 #[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq, PartialOrd, Ord, Eq, Hash)]
 #[repr(i32)]
-pub enum LimitPlayerSideMyNoSql {
-    Buyer = 0,  // Limit player on lows
-    Seller = 1, // Limit player on highs
+pub enum LimitTraderSideMyNoSql {
+    Buyer = 0,  // Limit Trader on lows
+    Seller = 1, // Limit Trader on highs
 }
 
-impl LimitPlayerSideMyNoSql {
+impl LimitTraderSideMyNoSql {
     pub fn as_str(&self) -> &str {
         match self {
             Self::Buyer => "Buyer",
@@ -37,18 +37,18 @@ impl LimitPlayerSideMyNoSql {
     }
 }
 
-impl ToString for LimitPlayerSideMyNoSql {
+impl ToString for LimitTraderSideMyNoSql {
     fn to_string(&self) -> String {
         self.as_str().to_string()
     }
 }
 
-impl From<i32> for LimitPlayerSideMyNoSql {
+impl From<i32> for LimitTraderSideMyNoSql {
     fn from(value: i32) -> Self {
         match value {
-            0 => LimitPlayerSideMyNoSql::Buyer,
-            1 => LimitPlayerSideMyNoSql::Buyer,
-            _ => panic!("Invalid value '{}' for LimitPlayerSideMyNoSql", value,),
+            0 => LimitTraderSideMyNoSql::Buyer,
+            1 => LimitTraderSideMyNoSql::Buyer,
+            _ => panic!("Invalid value '{}' for LimitTraderSideMyNoSql", value,),
         }
     }
 }
