0c56d3897ddbb910bb3504d7d213241a8b8c4ecb
amigin <amigin@gmail.com>
2025-04-30 10:04:46 +0300

    Added System Flags

--- src/lib.rs
+++ src/lib.rs
@@ -14,15 +14,18 @@ mod atr_bounds_entity;
 pub use atr_bounds_entity::*;
 mod near_level_instrument_entity;
 pub use near_level_instrument_entity::*;
-pub mod consts;
-mod candles_with_similar_extremes_entity;
 mod candle_pattern_entity;
+mod candles_with_similar_extremes_entity;
+pub mod consts;
 
-pub use candles_with_similar_extremes_entity::*;
 pub use candle_pattern_entity::*;
+pub use candles_with_similar_extremes_entity::*;
 mod trend_entity;
 pub use trend_entity::*;
 mod trend_settings;
 pub use trend_settings::*;
 mod limit_trader;
-pub use  limit_trader::*;
+pub use limit_trader::*;
+
+mod system_flags_entity;
+pub use system_flags_entity::*;
---
+++ src/system_flags_entity.rs
@@ -0,0 +1,21 @@
+use serde::*;
+
+service_sdk::macros::use_my_no_sql_entity!();
+
+//Entity where we keep some statuses. For instance - that we imported candles
+
+// Crypto Candles are imported
+//PartitionKey = "cci" - Crypto candle is imported
+//RowKey = InstrumentId
+//value = NotUsed
+
+#[my_no_sql_entity("price-levels")]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SystemFlagsMyNoSqlEntity {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<String>,
+}
+
+impl SystemFlagsMyNoSqlEntity {
+    pub const CRYPTO_CANDLE_IS_IMPORTED: &'static str = "cci";
+}
