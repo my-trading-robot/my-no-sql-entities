78bc7142af001aad6744bad765dd86f9828fdaa6
amigin <amigin@gmail.com>
2026-01-13 12:44:52 +0200

    Fixes with AtrSettings

--- src/atr_settings_entity.rs
+++ src/atr_settings_entity.rs
@@ -5,10 +5,8 @@ service_sdk::macros::use_my_no_sql_entity!();
 #[my_no_sql_entity("atr-settings")]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AtrSettingsEntity {
-    pub duration_days: u64,
     pub percent: f64,
     pub candles_count: i32,
-    pub candles_limit: Option<usize>,
 }
 
 impl AtrSettingsEntity {
@@ -22,10 +20,8 @@ impl Default for AtrSettingsEntity {
             partition_key: Self::PARTITION_KEY.to_string(),
             row_key: Self::ROW_KEY.to_string(),
             time_stamp: Default::default(),
-            duration_days: 365,
             percent: 0.8,
-            candles_count: 5,
-            candles_limit: None,
+            candles_count: 365,
         }
     }
 }
