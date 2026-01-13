475f74153eb457c441ebe913c4ac91b3a90a4e36
amigin <amigin@gmail.com>
2026-01-13 12:18:45 +0200

    Plugged trading-robot-common

--- src/atr_value_entity.rs
+++ src/atr_value_entity.rs
@@ -1,45 +1,7 @@
 use serde::*;
+use trading_robot_common::CandleType;
 
 service_sdk::macros::use_my_no_sql_entity!();
-
-const MINUTE_KEY: &'static str = "1m";
-const MINUTE_5_KEY: &'static str = "5m";
-const HOUR_KEY: &'static str = "1h";
-const DAY_KEY: &'static str = "1d";
-const MONTH_KEY: &'static str = "1M";
-
-#[derive(Clone, Copy)]
-pub enum MyNoSqlCandleType {
-    Minute,
-    Minute5,
-    Hour,
-    Day,
-    Month,
-}
-impl MyNoSqlCandleType {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            Self::Minute => MINUTE_KEY,
-            Self::Minute5 => MINUTE_5_KEY,
-            Self::Hour => HOUR_KEY,
-            Self::Day => DAY_KEY,
-            Self::Month => MONTH_KEY,
-        }
-    }
-
-    pub fn from_str(src: &str) -> Self {
-        match src {
-            MINUTE_KEY => Self::Minute,
-            MINUTE_5_KEY => Self::Minute5,
-            HOUR_KEY => Self::Hour,
-            DAY_KEY => Self::Day,
-            MONTH_KEY => Self::Month,
-            _ => {
-                panic!("Invalid candle interval key: {}", src);
-            }
-        }
-    }
-}
 
 //Partition - Instrument_id
 //RowKey - Interval ("1m", "5m", "1h", "1d", "1M")
@@ -54,7 +16,7 @@ impl AtrValueMyNoSqlEntity {
         instrument_id
     }
 
-    pub fn generate_row_key(candle_type: MyNoSqlCandleType) -> &'static str {
+    pub fn generate_row_key(candle_type: CandleType) -> &'static str {
         candle_type.as_str()
     }
 
@@ -62,7 +24,7 @@ impl AtrValueMyNoSqlEntity {
         &self.partition_key
     }
 
-    pub fn get_candle_type(&self) -> MyNoSqlCandleType {
-        MyNoSqlCandleType::from_str(&self.row_key)
+    pub fn get_candle_type(&self) -> CandleType {
+        CandleType::from_str(&self.row_key)
     }
 }
